c0925768789c8f118f02ad15f812fecf419401a9
黄永好 <jkddg@126.com>
2023-01-30 16:48:14 +0800

    加入定时抓图定时器

--- src/main/java/com/jkddg/nvrmailclient/service/MailTask.java
+++ src/main/java/com/jkddg/nvrmailclient/service/MailTask.java
@@ -1,7 +1,21 @@
 package com.jkddg.nvrmailclient.service;
 
+import com.jkddg.nvrmailclient.constant.SDKConstant;
+import com.jkddg.nvrmailclient.hkHelper.ChannelHelper;
+import com.jkddg.nvrmailclient.model.ChannelInfo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 
 /**
@@ -13,21 +27,23 @@ import org.springframework.stereotype.Component;
 public class MailTask {
 
 
-//    @Autowired
-//    MailService mailService;
+    @Autowired
+    AlarmService alarmService;
 
 
-//    @Scheduled(fixedRate = 5 * 1000)   //定时器定义，设置执行时间 5s
-//    @Async("taskPoolExecutor")
-//    public void checkMailSend() {
-//        mailService.checkAndSendMail();
-//
-//    }
+    @Scheduled(fixedRate = 5 * 60 * 1000)   //定时器定义，设置执行时间 5s
+    @Async("taskPoolExecutor")
+    public void timerMailSend() {
+        List<ChannelInfo> list = ChannelHelper.getOnLineIPChannels(SDKConstant.lUserID);
+        List<Integer> channels = list.stream().map(ChannelInfo::getNumber).collect(Collectors.toList());
+        alarmService.alarmAppendQueue(channels);
 
-//    @Bean(name = "taskPoolExecutor")
-//    public ExecutorService taskPoolExecutor() {
-//        ExecutorService poolExecutor = new ThreadPoolExecutor(2, 4, 10L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(), new ThreadPoolExecutor.CallerRunsPolicy());
-//        return poolExecutor;
-//    }
+    }
+
+    @Bean(name = "taskPoolExecutor")
+    public ExecutorService taskPoolExecutor() {
+        ExecutorService poolExecutor = new ThreadPoolExecutor(2, 4, 10L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(), new ThreadPoolExecutor.CallerRunsPolicy());
+        return poolExecutor;
+    }
 
 }
