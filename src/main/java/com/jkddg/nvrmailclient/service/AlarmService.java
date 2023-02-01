e9de5a741d398943feb4c81bcc3f614b7109ebdc
黄永好 <jkddg@126.com>
2023-02-01 15:40:38 +0800

    去掉锁

--- src/main/java/com/jkddg/nvrmailclient/service/AlarmService.java
+++ src/main/java/com/jkddg/nvrmailclient/service/AlarmService.java
@@ -29,7 +29,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 @Component
 public class AlarmService {
 
-    static Map<Integer, AlarmLockObject> lockMap = new ConcurrentHashMap<>();
+    //    static Map<Integer, AlarmLockObject> lockMap = new ConcurrentHashMap<>();
     public static Queue<AlarmMailInfo> ALARM_QUEUE = new LinkedBlockingQueue<>();
 
     static Map<Integer, LocalDateTime> alarmTimeMap = new HashMap<>();//key=通道号，value=上次预警时间
@@ -53,73 +53,67 @@ public class AlarmService {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        if (!lockMap.containsKey(channel)) {
-                            lockMap.put(channel, new AlarmLockObject(channel));
-                        }
-                        AlarmLockObject lockObject = lockMap.get(channel);
-                        synchronized (lockObject) {
-                            //判断预警间隔，太短的丢弃
-                            if (!alarmTimeMap.containsKey(channel)) {
-                                alarmTimeMap.put(channel, LocalDateTime.now());
-                            } else {
-                                LocalDateTime lastTime = alarmTimeMap.get(channel);
-                                LocalDateTime compareTime = LocalDateTime.now().minusSeconds(NvrConfigConstant.alarmIntervalSecond);
-                                if (lastTime.isAfter(compareTime)) {
+                        //判断预警间隔，太短的丢弃
+                        if (!alarmTimeMap.containsKey(channel)) {
+                            alarmTimeMap.put(channel, LocalDateTime.now());
+                        } else {
+                            LocalDateTime lastTime = alarmTimeMap.get(channel);
+                            LocalDateTime compareTime = LocalDateTime.now().minusSeconds(NvrConfigConstant.alarmIntervalSecond);
+                            if (lastTime.isAfter(compareTime)) {
 //                            log.info("通道名：" + channelInfo.getName() + "，通道号：" + channel + "预警间隔不够" + NvrConfigConstant.alarmIntervalSecond + "秒，丢弃");
-                                    return;
-                                }
-                            }
-                            //1、判断通道是否在线
-                            ChannelInfo channelInfo = ChannelHelper.getOnlineChannelInfoByNo(channel);
-                            if (channelInfo == null) {
-                                log.warn("预警通道不在线，通道号：" + channel);
-                                ChannelHelper.flashChannel();
                                 return;
                             }
-                            log.info("通道[" + channelInfo.getName() + "]触发抓图事件");
-                            //2、通道截图
-                            List<String> fileAttachments = new ArrayList<>();
-                            List<MailStreamAttachment> streamAttachments = new ArrayList<>();
-                            String picPrefix = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss-"));
-                            for (int i = 0; i < NvrConfigConstant.captureCount; i++) {
-                                if (NvrConfigConstant.captureInMemory) {
-                                    //内存抓图
-                                    MailStreamAttachment streamAttachment = capturePictureHelper.getMemoryImage(picPrefix + (i + 1), channelInfo);
-                                    if (streamAttachment != null) {
-                                        streamAttachments.add(streamAttachment);
-                                    } else {
-                                        log.warn(channelInfo.getName() + "第" + (i + 1) + "次内存抓图失败");
-                                    }
+                        }
+                        //1、判断通道是否在线
+                        ChannelInfo channelInfo = ChannelHelper.getOnlineChannelInfoByNo(channel);
+                        if (channelInfo == null) {
+                            log.warn("预警通道不在线，通道号：" + channel);
+                            ChannelHelper.flashChannel();
+                            return;
+                        }
+                        log.info("通道[" + channelInfo.getName() + "]触发抓图事件");
+                        //2、通道截图
+                        List<String> fileAttachments = new ArrayList<>();
+                        List<MailStreamAttachment> streamAttachments = new ArrayList<>();
+                        String picPrefix = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss-"));
+                        for (int i = 0; i < NvrConfigConstant.captureCount; i++) {
+                            if (NvrConfigConstant.captureInMemory) {
+                                //内存抓图
+                                MailStreamAttachment streamAttachment = capturePictureHelper.getMemoryImage(picPrefix + (i + 1), channelInfo);
+                                if (streamAttachment != null) {
+                                    streamAttachments.add(streamAttachment);
                                 } else {
-                                    //文件抓图
-                                    String imagePath = capturePictureHelper.getFileImage(picPrefix + (i + 1), channelInfo);
-                                    if (StringUtils.isEmpty(imagePath)) {
-                                        log.warn(channelInfo.getName() + "第" + (i + 1) + "次文件抓图失败");
-                                    } else {
-                                        fileAttachments.add(imagePath);
-                                    }
+                                    log.warn(channelInfo.getName() + "第" + (i + 1) + "次内存抓图失败");
                                 }
-                                if ((i + 1) < NvrConfigConstant.captureCount) {
-                                    try {
-                                        Thread.sleep(NvrConfigConstant.captureIntervalSecond * 1000);
-                                    } catch (InterruptedException e) {
-                                        throw new RuntimeException(e);
-                                    }
+                            } else {
+                                //文件抓图
+                                String imagePath = capturePictureHelper.getFileImage(picPrefix + (i + 1), channelInfo);
+                                if (StringUtils.isEmpty(imagePath)) {
+                                    log.warn(channelInfo.getName() + "第" + (i + 1) + "次文件抓图失败");
+                                } else {
+                                    fileAttachments.add(imagePath);
                                 }
                             }
-                            alarmTimeMap.put(channel, LocalDateTime.now());
-                            AlarmMailInfo alarmMailInfo = new AlarmMailInfo();
-                            alarmMailInfo.setChannel(channelInfo);
-                            if (!CollectionUtils.isEmpty(fileAttachments)) {
-                                alarmMailInfo.setFileImages(fileAttachments);
+                            if ((i + 1) < NvrConfigConstant.captureCount) {
+                                try {
+                                    Thread.sleep(NvrConfigConstant.captureIntervalSecond * 1000);
+                                } catch (InterruptedException e) {
+                                    throw new RuntimeException(e);
+                                }
                             }
-                            if (!CollectionUtils.isEmpty(streamAttachments)) {
-                                alarmMailInfo.setStreamImages(streamAttachments);
-                            }
-                            if (!CollectionUtils.isEmpty(streamAttachments) || !CollectionUtils.isEmpty(fileAttachments)) {
-                                ALARM_QUEUE.add(alarmMailInfo);
-                                mailService.checkAndSendMail();
-                            }
+                        }
+                        alarmTimeMap.put(channel, LocalDateTime.now());
+                        AlarmMailInfo alarmMailInfo = new AlarmMailInfo();
+                        alarmMailInfo.setChannel(channelInfo);
+                        if (!CollectionUtils.isEmpty(fileAttachments)) {
+                            alarmMailInfo.setFileImages(fileAttachments);
+                        }
+                        if (!CollectionUtils.isEmpty(streamAttachments)) {
+                            alarmMailInfo.setStreamImages(streamAttachments);
+                        }
+                        if (!CollectionUtils.isEmpty(streamAttachments) || !CollectionUtils.isEmpty(fileAttachments)) {
+                            ALARM_QUEUE.add(alarmMailInfo);
+                            mailService.checkAndSendMail();
                         }
                     }
                 }).start();
